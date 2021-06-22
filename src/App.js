7136f3d0ae6791b6c19fc3e788f7c27bf85d5aad
eugenehan94 <eugene.han94@gmail.com>
2021-06-22 19:49:42 -0400

    More useReducer Practice

--- src/App.js
+++ src/App.js
@@ -6,18 +6,46 @@ const initialState = {
 };
 
 const reducer = (state, action) => {
-  switch(action.type) {
-    case 
+  switch (action.type) {
+    case "increment1":
+      return { ...state, count1: state.count1 + 1 };
+    case "decrement1":
+      return { ...state, count1: state.count1 - 1 };
+    case "set1":
+      return { ...state, count1: action.count };
+    case "increment2":
+      return { ...state, count2: state.count2 + 1 };
+    case "decrement2":
+      return { ...state, count2: state.count2 - 1 };
+    case "set2":
+      return { ...state, count2: action.count };
+    default:
+      throw new Error("unexpected action");
   }
-}
-
+};
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <>
       <h1>Title</h1>
+      <div>
+        {state.count1}
+        <button onClick={() => dispatch({ type: "increment1" })}>+1</button>
+        <button onClick={() => dispatch({ type: "decrement1" })}>-1</button>
+        <button onClick={() => dispatch({ type: "set1", count: 0 })}>
+          Reset
+        </button>
+      </div>
+      <div>
+        {state.count2}
+        <button onClick={() => dispatch({ type: "increment2" })}>+1</button>
+        <button onClick={() => dispatch({ type: "decrement2" })}>-1</button>
+        <button onClick={() => dispatch({ type: "set2", count: 0 })}>
+          Reset
+        </button>
+      </div>
     </>
   );
 }
