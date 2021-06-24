db1477c2153295d1279a34ae4ff0d7f0bed54259
eugenehan94 <eugene.han94@gmail.com>
2021-06-24 17:11:43 -0400

    React Router Practice

--- src/App.js
+++ src/App.js
@@ -1,47 +1,27 @@
 import React, { useState, useReducer, useRef, useContext } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const someContext = React.createContext();
-
+import Home from "./Home";
+import About from "./About";
+import Error from "./Error";
+import Navbar from "./Navbar";
 function App() {
-  const [number, setNumber] = useState(541);
-
   return (
-    <>
-      <someContext.Provider value={number}>
-        <h1>Title</h1>
-        <button onClick={() => setNumber(number + 1)}>Increase Number</button>
-        <Level1 />
-      </someContext.Provider>
-    </>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/About">
+          <About />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
-
-const Level1 = () => {
-  return (
-    <>
-      <h2>This is level 1</h2>
-      <Level2></Level2>
-    </>
-  );
-};
-
-const Level2 = () => {
-  return (
-    <>
-      <h2>This is level 2</h2>
-      <Level3 />
-    </>
-  );
-};
-
-const Level3 = () => {
-  const number = useContext(someContext);
-  return (
-    <>
-      <h2>This is level 3</h2>
-      <p>This is the data: {number}</p>
-    </>
-  );
-};
