7aabdbb48214a2ae856e4b5f0c44bf2568a92fe6
eugenehan94 <eugene.han94@gmail.com>
2021-06-25 14:45:50 -0400

    React Router useParam and useRouteMatch practice

--- src/App.js
+++ src/App.js
@@ -1,27 +1,89 @@
 import React, { useState, useReducer, useRef, useContext } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Home from "./Home";
-import About from "./About";
-import Error from "./Error";
-import Navbar from "./Navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+// import Home from "./Home";
+// import About from "./About";
+// import Error from "./Error";
+// import Navbar from "./Navbar";
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/About">
-          <About />
-        </Route>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
 
 export default App;
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Topics() {
+  let { path, url } = useRouteMatch();
+  console.log(path);
+  console.log(url);
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/rendering`}>Rendering with react</Link>
+        </li>
+        <li>
+          <Link to={`${url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  console.log(topicId);
+  return (
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
