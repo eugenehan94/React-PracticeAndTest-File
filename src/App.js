1d609d30fd3fc72414ed0476251624338c70b825
eugenehan94 <eugene.han94@gmail.com>
2021-07-11 23:25:27 -0400

    loading animation

--- src/App.js
+++ src/App.js
@@ -7,31 +7,14 @@ import {
   useParams,
   useRouteMatch,
 } from "react-router-dom";
-// import Home from "./Home";
-// import About from "./About";
-// import Error from "./Error";
-// import Navbar from "./Navbar";
+
+import "./App.css";
+
 function App() {
-  const refContainer = useRef(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(refContainer.current.value);
-  };
-
-  console.log("This is outside the return " + refContainer.current);
-  console.log(refContainer.current === null);
   return (
     <div>
       <h1>Title</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input type="text" ref={refContainer}></input>
-          <button type="sumbit">
-            {refContainer.current === null ? "refContainer is null" : "submit"}
-          </button>
-        </div>
-      </form>
+      <div className="loader"></div>
     </div>
   );
 }
