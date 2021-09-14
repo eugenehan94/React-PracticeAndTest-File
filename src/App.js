d8e90444c7c9bc7b238f697374f93391ed206da5
eugenehan94 <eugene.han94@gmail.com>
2021-09-14 19:13:51 -0400

    Testing out code

--- src/App.js
+++ src/App.js
@@ -1,75 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { useFetch } from "./useFetch";
-import Follower from "./Follower";
+import { useState } from "react";
+import Splash from "./Components/SplashScreen";
+import { ThemeProvider } from "styled-components";
+
+const LightTheme = {
+  pageBackground: "white",
+  titleColor: "#dc658b",
+  tagLineColor: "black",
+};
+
+const DarkTheme = {
+  pageBackground: "#282c36",
+  titleColor: "lightpink",
+  tagLineColor: "lavender",
+};
+
+const themes = {
+  light: LightTheme,
+  dark: DarkTheme,
+};
+
 function App() {
-  const { loading, data } = useFetch();
-  const [page, setPage] = useState(0);
-  const [followers, setFollowers] = useState([]);
-
-  useEffect(() => {
-    if (loading) return;
-
-    setFollowers(data[page]);
-  }, [loading, page]);
-
-  const nextPage = () => {
-    setPage((oldPage) => {
-      let nextPage = oldPage + 1;
-      if (nextPage > data.length - 1) {
-        nextPage = 0;
-      }
-      return nextPage;
-    });
-  };
-  const prevPage = () => {
-    setPage((oldPage) => {
-      let prevPage = oldPage - 1;
-      if (prevPage < 0) {
-        prevPage = data.length - 1;
-      }
-      return prevPage;
-    });
-  };
-
-  const handlePage = (index) => {
-    setPage(index);
-  };
-
+  const [theme, setTheme] = useState("light");
   return (
-    <main>
-      <div className="section-title">
-        <h1>{loading ? "loading..." : "pagination"}</h1>
-        <div className="underline"></div>
-      </div>
-      <section className="followers">
-        <div className="container">
-          {followers.map((follower) => {
-            return <Follower key={follower.id} {...follower} />;
-          })}
-        </div>
-        {!loading && (
-          <div className="btn-container">
-            <button className="prev-btn" onClick={prevPage}>
-              prev
-            </button>
-            {data.map((item, index) => {
-              return (
-                <button
-                  key={index}
-                  className={`page-btn ${index === page ? "active-btn" : null}`}
-                  onClick={() => handlePage(index)}
-                >
-                  {index + 1}
-                </button>
-              );
-            })}
-            <button className="next-btn" onClick={nextPage}>
-              next
-            </button>
-          </div>
-        )}
-      </section>
-    </main>
+    <ThemeProvider theme={themes[theme]}>
+      <Splash theme={theme} setTheme={setTheme} />
+    </ThemeProvider>
   );
 }
 
