f4bfa6d8e712634d0beeace36c48da5921586a77
eugenehan94 <eugene.han94@gmail.com>
2021-10-31 20:00:48 -0400

    Testing dialog tag of material ui

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
-import Snackbar from "@material-ui/core/Snackbar";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import ListItemText from "@mui/material/ListItemText";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
 
-export default function PositionedSnackbar() {
-  const [state, setState] = React.useState({
-    open: false,
-    vertical: "top",
-    horizontal: "center",
-  });
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
 
-  const { vertical, horizontal, open } = state;
+export default function FullScreenDialog() {
+  const [open, setOpen] = React.useState(false);
 
-  const handleClick = (newState) => () => {
-    setState({ open: true, ...newState });
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
   const handleClose = () => {
-    setState({ ...state, open: false });
+    setOpen(false);
   };
-
-  const buttons = (
-    <React.Fragment>
-      <Button
-        onClick={handleClick({ vertical: "bottom", horizontal: "center" })}
-      >
-        Bottom-Center
-      </Button>
-    </React.Fragment>
-  );
 
   return (
     <div>
-      {buttons}
-      <Snackbar
-        anchorOrigin={{ vertical, horizontal }}
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open full-screen dialog
+      </Button>
+      <Dialog
+        fullScreen
         open={open}
         onClose={handleClose}
-        message="I love snacks"
-        key={vertical + horizontal}
-      />
+        TransitionComponent={Transition}
+      >
+        {/* <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Sound
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button>
+          </Toolbar>
+        </AppBar> */}
+        {/* <List>
+          <ListItem button>
+            <ListItemText primary="Phone ringtone" secondary="Titania" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText
+              primary="Default notification ringtone"
+              secondary="Tethys"
+            />
+          </ListItem>
+        </List> */}
+      </Dialog>
     </div>
   );
 }
