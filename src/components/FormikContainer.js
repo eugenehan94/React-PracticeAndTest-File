2cc46fcdff39a7aaf9948b6e22bd9471682731db
eugenehan94 <eugene.han94@gmail.com>
2021-11-20 15:56:08 -0500

    more Formik practice

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -3,11 +3,13 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import FormikControl from "./FormikControl";
 function FormikContainer() {
-  const initialValues = {};
+  const initialValues = {
+    email: "",
+  };
   const validationSchema = Yup.object({
     email: Yup.string().required("Required"),
   });
-  onSubmit = (values) => console.log("Form data", values);
+  const onSubmit = (values) => console.log("Form data", values);
   return (
     <Formik
       initialValues={initialValues}
