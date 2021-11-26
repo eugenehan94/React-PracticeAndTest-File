464947e4ff3cbdb19afe705b6f116f1608f93db1
eugenehan94 <eugene.han94@gmail.com>
2021-11-25 21:14:53 -0500

    Material ui and Formik

--- src/App.js
+++ src/App.js
@@ -1,18 +1,90 @@
-// import YoutubeForm from "./components/YoutubeForm";
-import FormikContainer from "./components/FormikContainer";
-import EnrollmentForm from "./components/EnrollmentForm";
-import { theme, ThemeProvider } from "@chakra-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Container,
+  Grid,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import Textfield from "./Component/TextField/";
+const useStyles = makeStyles((theme) => ({
+  fontWrapper: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(8),
+  },
+}));
+const INITIAL_FORM_STATE = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+};
+
+const FORM_VALIDATION = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  phone: Yup.number()
+    .integer()
+    .typeError("Please enter a valid number")
+    .required("Required"),
+});
 const App = () => {
+  const classes = useStyles();
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <h1>Title</h1>
-        {/* <YoutubeForm /> */}
-        <FormikContainer />
-        <h2>EnrollmentForm</h2>
-        <EnrollmentForm />
-      </div>
-    </ThemeProvider>
+    <div>
+      <Grid container>
+        <Grid item xs={12}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h4">Heading</Typography>
+            </Toolbar>
+          </AppBar>
+        </Grid>
+        <Grid item xs={12}>
+          <Container maxWidth="md">
+            <div className={classes.formWrapper}>
+              <Formik
+                initialValues={{ ...INITIAL_FORM_STATE }}
+                validationSchema={FORM_VALIDATION}
+                onSubmit={(values) => {
+                  console.log(values);
+                }}
+              >
+                <Form>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <Typography>Your details</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Textfield name="firstName" label="First Name" />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Textfield name="lastName" label="Last Name" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography>Address</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Textfield name="email" label="Email" />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography>Booking Information</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Textfield name="phone" label="Phone" />
+                    </Grid>
+                  </Grid>
+                </Form>
+              </Formik>
+            </div>
+          </Container>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
--- src/components/TextError.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TextError(props) {
-  return <div style={{ color: "red" }}>{props.children}</div>;
-}
-
-export default TextError;
